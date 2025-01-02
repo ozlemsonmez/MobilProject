550b32e383642c4bfe7aed1473fcfd799839a894
melekball <melekbal@bluearf.com>
2025-01-02 18:57:32 +0300

    UI updates 5

---
+++ lib/entry_page/entry_page_model.dart
@@ -0,0 +1,20 @@
+import '/flutter_flow/flutter_flow_animations.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:math';
+import 'dart:ui';
+import 'entry_page_widget.dart' show EntryPageWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class EntryPageModel extends FlutterFlowModel<EntryPageWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
