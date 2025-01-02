d5a20733cf08d32fff65a3e357dea3ba084f8608
melekball <melekbal@bluearf.com>
2025-01-02 15:22:00 +0300

    UI updates 4

---
+++ lib/components/allertpopup_model.dart
@@ -0,0 +1,16 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import 'allertpopup_widget.dart' show AllertpopupWidget;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class AllertpopupModel extends FlutterFlowModel<AllertpopupWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
